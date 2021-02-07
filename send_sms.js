00c79c9363ea8c6a9b7dfb0ba73d879785205057
viraphilavong <alexander.philavong.0640@gmail.com>
2021-02-06 20:38:20 -0600

    Remove hardcoded twilio information

--- send_sms.js
+++ send_sms.js
@@ -1,14 +1,15 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-const accountSid = "ACcaa85cc50f8b0e53be92b6ae2c1a064c";
-const authToken = "8bf6255e62166bccc8590efd86de3d11";
+require('dotenv').config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: "+19094734884",
+     from: process.env.TWILIO_NUMBER,
      to: '+16125590640'
    })
   .then(message => console.log(message.sid));
